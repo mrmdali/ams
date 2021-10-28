9259edd6a411ae2b3f263073bfc6c353929f2cfe
root <root@hosting.ip.citynet.uz>
2021-10-28 14:42:25 +0500

    #.27

--- amspage/settings.py
+++ amspage/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-37f4uxnnh^=uwa(e+4lemxa$)98wite--9&%rt(pv$pwpj44e9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
